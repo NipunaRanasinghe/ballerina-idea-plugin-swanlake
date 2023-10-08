65f6c2f060c7a98d96875b70b21e1863a1aa479c
GitHub Action <action@github.com>
2023-10-08 09:39:33 +0000

    Template cleanup

--- src/main/kotlin/com/github/nipunaranasinghe/ballerinapluginswanlake/services/MyProjectService.kt
+++ src/main/kotlin/com/github/nipunaranasinghe/ballerinapluginswanlake/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.nipunaranasinghe.ballerinapluginswanlake.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.nipunaranasinghe.ballerinapluginswanlake.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
